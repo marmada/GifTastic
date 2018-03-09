4249e6719c79b137e498e579bb35213cd317aab6
Miriam Armada <miriam_armada@yahoo.com>
2018-03-09 10:28:55 -0700

    update

--- javascript/main.js
+++ javascript/main.js
@@ -3,17 +3,16 @@ var foodArr = ["bbq", "french food", "beans", "mexican food", "pie", "cake", "ch
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hvnwkL6roszPGAdklacfXfRWFssZNqLM&limit=10&offset=0&rating=G&lang=en&q=" + "XXXXXX"
 
 
-$.ajax({
+/*$.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     $("XXXXXXXX").text(JSON.stringify(response));
-});
-
+});*/
 function createButtons() {
 
     //empty buttons
-    $("#foodButton").empty();
+    $("#foodButtons").empty();
 
     // Loop thru array
     for (var i = 0; i < foodArr.length; i++) {
@@ -21,13 +20,34 @@ function createButtons() {
         //create button
         var b = $("<button>");
         
-        b.addClass("food");
-        b.addClass()
+        b.addClass("food btn btn-info");
         // Adding a data-attribute
         b.attr("data-name", foodArr[i]);
         // Add Value/Text
-        a.text(foodArr[i]);
+        b.text(foodArr[i]);
         // Adding the button to the HTML
-        $("#foodButton").append(b);
+        $("#foodButtons").append(b);
     }
 }
+
+$("#add-newFood").on("click", function(event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var food = $("#food-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    food.push(foodArr);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+
+  });
+
+
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    createButtons();
+});

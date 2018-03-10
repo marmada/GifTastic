66cd8025e6975794015448eba9b2fda2fd1abdb4
Miriam Armada <miriam_armada@yahoo.com>
2018-03-10 09:54:14 -0700

    update

--- javascript/main.js
+++ javascript/main.js
@@ -2,20 +2,18 @@
 
 var foodArr = ["bbq", "french food", "beans", "mexican food", "pie", "cake", "chocolate", "steak", "pho", "paella"];
 
-var foodQ= "";
+$("#foodButtons").on("click","button", function () {
 
-$("button").on("click", function() {
-
-    foodQ = $(this).attr("data-name");
+  foodQ = $(this).attr("data-name");
 
     console.log(foodQ);
 
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?&q=" 
-    + foodQ + "api_key=hvnwkL6roszPGAdklacfXfRWFssZNqLM&limit=12&offset=0&rating=PG&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hvnwkL6roszPGAdklacfXfRWFssZNqLM&q="+ foodQ +"&limit=10&offset=0&rating=G&lang=en";
 
     console.log(queryURL);
-    // Performing an AJAX request with the queryURL
+
+    //Get AJAX request with the queryURL
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -27,6 +25,7 @@ $("button").on("click", function() {
             // storing the data from the AJAX request in the results variable
             var results = response.data;
 
+
             // Looping through each result item
             for (var i = 0; i < results.length; i++) {
 
@@ -36,8 +35,8 @@ $("button").on("click", function() {
 
                 // add subdiv per each Giphy
 
-                var foodDiv= $("<div class='col-3'>");
-                
+                var foodDiv = $("<div class='col-3'>");
+
                 // add raiting
 
                 var p = $("<p>").text("Rating: " + results[i].rating);
@@ -48,11 +47,12 @@ $("button").on("click", function() {
 
                 // add Attributes
 
-            
+
                 foodGiphy.attr("src", results[i].images.fixed_height_still.url);
                 foodGiphy.attr("data-state", "still");
                 foodGiphy.attr("data-still", results[i].images.fixed_height_still.url);
-                foodGiphy.attr("data-animate", results[i].images.fixed_width.url );
+                foodGiphy.attr("data-animate", results[i].images.fixed_width.url);
+             
 
                 // add p & gif to div
                 foodDiv.append(p);
@@ -73,9 +73,11 @@ function createButtons() {
     for (var i = 0; i < foodArr.length; i++) {
 
         //create button
-        var b = $("<button>");
+        var b = $("<button type='button'>");
 
-        b.addClass("food btn btn-info");
+        b.addClass("foodQ");
+
+        b.addClass("btn btn-info");
         // Adding a data-attribute
         b.attr("data-name", foodArr[i]);
         // Add Value/Text
@@ -93,6 +95,10 @@ $("#add-newFood").on("click", function (event) {
 
     // Adding the movie from the textbox to our array
     foodArr.push(food);
+
+    $("#food-input").empty();
+
+    
 
     // Calling renderButtons which handles the processing of our movie array
     createButtons();
